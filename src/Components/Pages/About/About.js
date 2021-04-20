c603c7760f4ca2679cf9740a29053a84d0537cc2
Wade Pate <wadejp8@gmail.com>
2021-04-20 07:26:13 -0600

    stash changes

--- src/Components/Pages/About/About.js
+++ src/Components/Pages/About/About.js
@@ -18,32 +18,35 @@ function About() {
 
       <br/>
         <p className='current-description'>
-          I'm a Web Developer that absolutely loves learning new things, I'm extremely dedicated to being the best me everything I do and strive to help others. I'm very blessed to have an amazing wife and lovely daughter who have always been there and I would do anything for. We currently live in North Dakota due to my previous line of work in the Oil and Gas industry, but I've held such a passion for code since High School but never truly pursued it for a career. Instead I dabbled on the side and read many books taking any opportunity I could to deepen my knowledge and understanding of computer language.
+          I'm a Web Developer that absolutely loves learning new things, I'm extremely dedicated to being the best me in everything I do and strive to help others. 
           <br/>
           <br/>
-          My family and I are eager to relocate...
-          {/* <br/>
+          I'm very blessed to have an amazing wife and lovely daughter who have always been there and that I would do anything for. We currently live in North Dakota due to my previous line of work in the Oil and Gas industry, but I've held such a passion for code since High School but never truly pursued it for a career.
           <br/>
-          Right now we're looking to move to Salt Lake City, Utah but other options are 100% available for discussion! */}
+          <br/>
+          Instead I dabbled on the side and read many books taking any opportunity I could to deepen my knowledge and understanding of computer language.
+          <br/>
+          <br/>
+          I completed DevMountain boot camp on April 2nd, 2021 and I'm eager to put all of my verified skills into action and continue gaining incredible knowledge in the industry which I truly love!
         </p>
         <h3 className='page-title'>
-          My Goals
+          Goals
         </h3>
       <br/>
         <p className='text-description'>
-          I like to think my goals are rather simple but here's the run down and I'll let you be the judge!
+          Setting goals is a key towards further development in ones self.
         </p>
       <br/>
         <ul className='goal-list'>
           <li className='goal'>To become a valued asset to the company I'm representing</li>
           <hr/>
-          <li className='goal'>Always put my best effort into a project or finding solutions</li>
+          <li className='goal'>Always put my best effort into a project and finding solutions</li>
           <hr/>
           <li className='goal'>Create amazing UI/UX results for users</li>
           <hr/>
           <li className='goal'>Making things easier for people and helping them however possible</li>
           <hr/>
-          <li className='goal'>Build a solid foundation for myself in the company I'm with</li>
+          <li className='goal'>To build a solid foundation for company as well as self</li>
         </ul>
       </section>
     </div>
--- src/Components/Pages/Projects/Projects.js
+++ src/Components/Pages/Projects/Projects.js
@@ -11,40 +11,45 @@ function Projects() {
   return (
     <div id='projects'>
       <section className='page-title'>
-        <h2>Some of my projects!</h2>
+        <h2>Some of my Projects!</h2>
       </section>
 
       <section className='project-list'>
-      <div className='project-container'>
-          <p className='project-title underline'>
-            BillTrax
-          </p>
-          <p className='project-summary'>
-            Where users can keep track of their personal finances and budget costs.
-          </p>
-          <img className='project-image' src={process.env.PUBLIC_URL + '/img/BillTrax.png'} alt='BillTrax app' />
+        <div className='project-container'>
+          <a target='_blank' rel='noreferrer' href='https://billtrax.org/' >
+            <p className='project-title underline'>
+              BillTrax
+            </p>
+            <p className='project-summary'>
+              Where users can keep track of their personal finances and budget costs.
+            </p>
+            <img className='project-image' src={process.env.PUBLIC_URL + '/img/BillTrax.png'} alt='BillTrax app' />
+          </a>
         </div>
         <div className='project-container'>
-          <p className='project-title underline'>
-            Lorem Cakes
-          </p>
-          <p className='project-summary'>
-            A simple mock-up for a checkout cart, created with React and no Database
-          </p>
-          <img className='project-image' src={process.env.PUBLIC_URL + '/img/LoremCakes.png'} alt='Lorem Cakes app' />
+          <a target='_blank' rel='noreferrer' href='https://github.com/Dozr13/lorem-cakes'>
+
+            <p className='project-title underline'>
+              Lorem Cakes
+            </p>
+            <p className='project-summary'>
+              A simple mock-up for a checkout cart, created with React and no Database
+            </p>
+            <img className='project-image' src={process.env.PUBLIC_URL + '/img/LoremCakes.png'} alt='Lorem Cakes app' />
+          </a>
         </div>
         <div className='project-container'>
-          <p className='project-title underline'>
-            G.R.C.
-          </p>
-          <p className='project-summary'>
-            Git-React Comps is a group project where you can upload and retrieve Component designs.
-          </p>
-          <img className='project-image' src={process.env.PUBLIC_URL + '/img/GRC.png'} alt='Git-React Comps app' />
+          <a target='_blank' rel='noreferrer' href='https://github.com/GitReactComponents'>
+            <p className='project-title underline'>
+              G.R.C.
+            </p>
+            <p className='project-summary'>
+              Git-React Comps is a group project where you can upload and retrieve Component designs.
+            </p>
+            <img className='project-image' src={process.env.PUBLIC_URL + '/img/GRC.png'} alt='Git-React Comps app' />
+          </a>
         </div>
-        
       </section>
-      
     </div>
   )
 }
